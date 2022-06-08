30805cf83cf80a9a2372f9df4ffe992362523d05
Marina Gerasymova <m.gerasymova29@gmail.com>
2022-06-08 20:50:36 +0300

    feat(firebase functions): add firebase functions expample

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,19 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
 
-Vue.use(Vuetify);
+import colors from 'vuetify/lib/util/colors'
+
+Vue.use(Vuetify)
 
 export default new Vuetify({
-});
+  theme: {
+    themes: {
+      light: {
+        primary: colors.blue.darken2, // #E53935
+        secondary: colors.red.lighten4, // #FFCDD2
+        accent: colors.indigo.base, // #3F51B5
+        error: colors.red.darken1
+      },
+    },
+  },
+})
