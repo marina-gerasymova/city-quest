035255d8a66de1e9f6d5075a2a19836a1f846df5
Marina Gerasymova <m.gerasymova29@gmail.com>
2022-06-12 19:19:04 +0300

    feature(basic components): button, footer

---
+++ src/components/Button.vue
@@ -0,0 +1,36 @@
+<template #button>
+  <div
+    class="button"
+    @click="$emit('button-click')"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/utils/colors";
+@import "@/style/utils/mixins";
+@import "@/style/utils/variable";
+
+.button {
+  width: max-content;
+  padding: 10px 20px;
+  color: $color-text-blue;
+  @include font-size-16px;
+  font-weight: bold;
+  background: $color-button-background;
+  border-radius: $border;
+  transition: all ease 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    background: $color-button-hover;
+  }
+}
+</style>
