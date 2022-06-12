d556b95afe444e6216dd7bc6932b1d7389638dd0
Marina Gerasymova <m.gerasymova29@gmail.com>
2022-06-13 00:54:54 +0300

    feat(player): added quest page

---
+++ src/components/Team.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="team">
+    <div class="team__title">TITLE</div>
+    <div class="team__list">
+      <div
+        class="team__person"
+        v-for="(person, index) in team"
+        :key="person.id"
+      >
+        {{ index }}. {{ person.name }}
+      </div>
+    </div>
+    <div class="team__button">
+      <Button
+        class="button"
+      >
+        Приєднатися
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from "@/components/Button.vue";
+
+export default {
+  name: "Team",
+  components: {
+    Button
+  },
+  props: {
+    team: {
+      type: Array,
+      default: () => {
+        return [
+          {
+            name: 'Maryna',
+            id: '1',
+          },
+          {
+            name: 'Daniil',
+            id: '2'
+          }
+        ]
+      },
+    }
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.team {
+  width: 100%;
+  font-size: 14px;
+
+  &__title {
+    margin-bottom: 5px;
+    text-align: center;
+  }
+  
+  &__person {
+    margin-bottom: 5px;
+  }
+
+  &__button {
+    width: 100%;
+    margin: 10px auto 0;
+  }
+
+  .button { 
+    margin: 0 auto;
+    font-size: 12px;
+  }
+}
+</style>
---
+++ src/views/PlayerQuestPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="page-size player-quest-page">
+    <div class="player-quest-page__title title">
+      <div>До початку гри залишилось:</div>
+      <div class="player-quest-page__start-time"></div>
+    </div>
+    <div class="player-quest-page__info">
+      <div class="player-quest-page__line">
+        <div>Час:</div>
+        <div>Hour</div>
+      </div>
+      <div class="player-quest-page__line">
+        <div>Мicце</div>
+        <div>Location</div>
+      </div>
+    </div>
+    <div class="player-quest-page__teams">
+      <div class="player-quest-page__title">Приєднатися до команди:</div>
+      <Team class="player-quest-page__team" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Team from "@/components/Team.vue";
+
+export default {
+  name: 'PlayerQuestPage',
+  components: {
+    Team
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/components/player-quest-page.scss";
+
+.page-size {
+  margin: 40px auto;
+}
+.button {
+  width: 100%;
+  margin: 20px auto;
+
+  &-wrapper {
+    width: 100%;
+  }
+}
+</style>
