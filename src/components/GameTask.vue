10ce25dba1458428cafad8e342cf28af84f8c15f
marina-gerasymova <m.gerasymova29@gmail.com>
2022-06-13 18:51:17 +0300

    feat(reading the task): reading task info with faas

--- src/components/GameTask.vue
+++ src/components/GameTask.vue
@@ -1,26 +1,57 @@
 <template>
-  <div class="game-task">
-    <div class="game-task__title">name</div>
-    <div class="game-task__image-wrap"></div>
-    <div class="game-task__description">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+  <div v-if="task" class="game-task">
+    <div class="game-task__title">{{ task.name }}</div>
+    <div class="game-task__image-wrap">
+      <img :src="task.imgUrl">
+    </div>
+    <div class="game-task__description">
+      {{ task.description }}
+    </div>
     <v-text-field
       class="cq-text-input"
-      v-model="email"
+      v-model="key"
       label="Відповідь"
       outlined
       required
     ></v-text-field>
-    <Button class="game-task__button">Відправити</Button>
+    <Button @click="sendKey" class="game-task__button">Відправити</Button>
   </div>
 </template>
 
 <script>
 import Button from '@/components/Button.vue';
+import { getFunctions, httpsCallable } from "firebase/functions";
+import { getApp } from 'firebase/app';
 
 export default {
   name: 'GameTask',
   components: {
     Button
+  },
+  data() {
+    return {
+      key: '',
+      task: null,
+      faasReadTask: httpsCallable(getFunctions(getApp()), 'readTask'),
+      faasTeamNextTask: httpsCallable(getFunctions(getApp()), 'nextTaskTeam'),
+    };
+  },
+  async mounted() {
+    const taskData = await this.faasReadTask({
+      questId: this.$route.params.code,
+      taskUid: this.$route.params.taskId
+    })
+
+    if (taskData.data) {
+      this.task = taskData.data.task;
+    } else {
+      this.$router.back();
+    }
+  },
+  methods: {
+    sendKey() {
+
+    }
   }
 }
 </script>
